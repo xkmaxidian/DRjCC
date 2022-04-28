6a8464317ca080ced153394c673fbb6e017d239f
unknown <xkma@xidian.edu.cn>
2022-04-28 14:42:14 +0800

    Update

--- drjcc.m
+++ drjcc.m
@@ -22,212 +22,107 @@ function [Z_final,A_final,B_final, F_final] = drjcc(X, k1, k2, W, options)
 %   tol1,tol2 Iteration error
 %   bSuccess
 %     [n,m]=size(X);
+    n = size(X,1);
+    m = size(X,2);
+    %%%%%%%%%%%%------Three initialization methods
+    %%%------Random initialization
+%     Z0= abs(rand(n,k1));
+%     A0 = abs(rand(k1,m));
+%     B0= abs(rand(k1,k2));
+%     F0 = abs(rand(k2,m));
 
-
-
-
-differror = 1e-5;
-if isfield(options,'error')
-    differror = options.error;
-end
-
-maxIter = [];
-if isfield(options, 'maxIter')
-    maxIter = options.maxIter;
-end
-
-nRepeat =1;
-if isfield(options,'nRepeat')
-    nRepeat = options.nRepeat;
-end
-
-minIterOrig = 30;
-if isfield(options,'minIter')
-    minIterOrig = options.minIter;
-end
-minIter = minIterOrig-1;
-
-meanFitRatio = 0.1;
-if isfield(options,'meanFitRatio')
-    meanFitRatio = options.meanFitRatio;
-end
-
-alpha = 1;
-if isfield(options,'alpha')
-    alpha = options.alpha;
-end
-%%%%%%===========Parameter settings===========
-Norm = 2;
-NormV = 1;
-
-
-if min(min(X)) < 0
-    error('Input should be nonnegative!');
-end
-
-[n,m]=size(X);
-Z0= abs(rand(n,k1));
-A0 = abs(rand(k1,m));
-[mFea,nSmp]=size(A0);
-
-%%%%%%=========== Weight matrix setting===========
-
-if isfield(options,'weight') && strcmpi(options.weight,'NCW')
-    feaSum = full(sum(X,2));
-    D_half = (X'*feaSum).^.5;
-    for i = 1:nSmp
-        X(:,i) = X(:,i)/D_half(i);
-    end
-end
-
-if isfield(options,'alpha_nSmp') && options.alpha_nSmp
-    alpha = alpha*nSmp;    
-end
-
-W = alpha*W;% Weight matrix
-DCol = full(sum(W,2));% Sum of row elements constitutes column vector DCol
-D = spdiags(DCol,0,speye(size(W,1)));% Compose Diagonal D
-L = D - W;
-if isfield(options,'NormW') && options.NormW
-    D_mhalf = DCol.^-.5;
-
-    tmpD_mhalf = repmat(D_mhalf,1,nSmp);
-    L = (tmpD_mhalf.*L).*tmpD_mhalf';
-    clear D_mhalf tmpD_mhalf;
-
-    L = max(L, L');
-end
-
-bSuccess.bSuccess = 1;
-
-%%%%%%%===========initialization================
-
+%%%------Initialization by SVD
+    [U,vV,D] = svds(X,k1);
+    Z0 = abs(U*sqrt(vV));
+    A0 = abs(sqrt(vV)*D');
     
-selectInit = 1;
-if ~exist('U','var')
-    B0 = abs(rand(k1,k2));
-    F0 = abs(rand(k2,m));
-else
-    nRepeat = 1;
-end
+    [U,vV,D] = svds(A0,k2);
+    B0 = abs(U*sqrt(vV));
+    F0 = abs(sqrt(vV)*D');
+    
+%%%------Initialization by NMF
+%     [Z0,A0] = snmf(X,k1,100);
+%     [B0,F0] = snmf(A0,k2,100);
+    
+    
+    D = diag(sum(W,2));
+    L = D-W;
+    
+    Z = Z0;A = A0;
+    B = B0;F = F0;
+    E = A;
+    T = zeros(k1,m);
+    iter = 0; 
+    converged = 0;    
+    maxIter = 100;  
+    tol1 = 1e-5;tol2 = 1e-5;
+    tryNo = 0;
+    w1 = 1;w2 = 1;
+    
+    %%%%%%%===========Update variables Z,A,B,F by iteration================
 
-[Z0,A0] = NormalizeUV(Z0, A0', NormV, Norm);A0=A0';
-[B0,F0] = NormalizeUV(B0, F0', NormV, Norm);F0=F0';
-
-Zk=Z0;Ak=A0;
-Bk=B0;Fk=F0;
-Ek=Ak;
-Tk= zeros(k1,nSmp);
-iter = 0; 
-converged = 0;    
-maxIter=100;  
-tol1=1e-5;tol2=1e-5;
-tryNo=0;
-w1=1;w2=1;
-
-%%%%%%%===========Update variables Z,A,B,F by iteration================
-
-  while ~converged  && iter < maxIter   
+      while ~converged  && iter < maxIter   
 %     tmp_T = cputime;
 %     tryNo = tryNo+1;
 %     nIter = 0;
 %     maxErr = 1;
 %     nStepTrial = 0;
     iter = iter + 1;
-        derta =5e+1;
+    derta = 5e+1;
 
-%%%%% Update variables Z and A, where Zk and Ak are the variables at the k-th iteration, 
-%%%%% and Zkl and Akl are the variables at the k+1-th iteration.==================
-        alpha1=norm(X,1)/norm(Zk,'fro');alpha2=norm(Ak,1)/norm(Fk,'fro');%%%%%%%Regularization parameter
-%          lambda1=norm(X,1)/norm(DTk,'fro');;lambda2=norm(Vk,2)/norm(Fk,'fro');
+    alpha1 = 0.5; alpha2 = 0.5;
         %%%========Update variables Z==========
-        Zkl=Zk.*((X*Ak')./(Zk*Ak*Ak'));
-        %%%========Update variables A==========
-        I=eye(k1);
-        VV1=w1*Zkl'*X+w2*Bk*Fk+derta*Ek+Tk;
-        VV2=(w1*Zkl'*Zkl+w2*I+derta*I)*Ak;
-        Akl=Ak.*(VV1./VV2);
-        Ekl=soft(Akl-Tk/derta,alpha1/derta);
-        Tkl=Tk+1.618*derta*(Ekl-Akl);
-%%%%%Update variables B and F, where Bk and Fk are the variables at the k-th iteration, 
-%%%%% and Bkl and Fkl are the variables at the k+1-th iteration.==================
-        %%%========Update variables B==========
-        Bkl=Bk.*(Akl*Fk')./(Bk*Fk*Fk');
-        for i=1:size(Bkl,1)
-            for j=1:size(Bkl,2)
-                if Bkl(i,j)<0
-                   Bkl(i,j)=0;
-                else
-                    Bkl(i,j)=Bkl(i,j);
-                end
-            end
-        end
-        %%%========Update variables F==========
-        FF1=w2*Bkl'*Akl+alpha2*Fk*W;
-        FF2=w2*Bkl'*Bkl*Fk+alpha2*Fk*D;
-        Fkl=Fk.*(FF1)./(FF2);
-        for i=1:size(Fkl,1)
-            for j=1:size(Fkl,2)
-                if Fkl(i,j)<0
-                   Fkl(i,j)=0;
-                else
-                    Fkl(i,j)=Fkl(i,j);
-                end
-            end
-        end
+    Zwk = Z;
+    Z = Z.*((X*A')./(Z*A*A'+eps));
+    Zkl = Z;
+    %%%========Update variables A==========
+    I = eye(k1);
+    VV1 = w1*Z'*X+w2*B*F+derta*E+T;
+    VV2 = (w1*Z'*Z+w2*I+derta*I)*A;
+    Awk = A;
+    A = A.*(VV1./VV2+eps);
+    Akl = A;
+    Ewk = E;
+    E = soft(A-T/derta,alpha1/derta);
+    Ekl = E;
+    T = T+1.618*derta*(E-A);
+    
+    %%%%%Update variables B and F, where Bk and Fk are the variables at the k-th iteration, 
+    %%%%% and Bkl and Fkl are the variables at the k+1-th iteration.==================
+    %%%========Update variables B==========
+    Bwk = B;
+    B = B.*(A*F')./(B*F*F'+eps);
+    B = max(B,0);
+    Bkl = B;
+    %%%========Update variables F==========
         
-%         [DTkl,Vkl] = NormalizeUV(DTkl, Vkl', NormV, Norm);Vkl=Vkl';
-        [Bkl,Fkl] = NormalizeUV(Bkl, Fkl', NormV, Norm);Fkl=Fkl';
-    
-    Zwk = Zk;
-    Awk = Ak;
-    Ewk=Ek;
-    Bwk = Bk;
-    Fwk = Fk;
-    
-    Zk=Zkl;
-    Ak=Akl;
-    Bk=Bkl;
-    Fk=Fkl;
-%%%%%%%%%% Error
-%   Er1(iter,:)=abs(mean(X - DTk*Vk))./norm(DTk*Vk,'fro');
-%   Er2(iter,:)=abs(mean(Vk - Bk*Fk))./norm( Bk*Fk,'fro');
-%   er1(iter)=mean(Er1(iter,:));
-%   er2(iter)=mean(Er2(iter,:));
-%   er=er1+er2;
-  
-  
+    Fwk = F;
+    FF1 = w2*B'*A+alpha2*F*W;
+    FF2 = w2*B'*B*F+alpha2*F*D;
+    F = F.*(FF1)./(FF2);
+    F = max(F,0);
+    Fkl = F;
+
     temp = max ([norm(Zkl-Zwk,'fro'),norm(Akl-Awk,'fro'),norm(Bkl-Bwk,'fro'),norm(Fkl-Fwk,'fro')]);
     %     temp = muu*temp/norm(V,2);
     temp =temp/max([norm(X,'fro')]);
-    %     temp = max([(sqrt(L)*norm(ZK-Zkm1,'fro')),norm(WK-Wkm1,'fro'),norm(EK-Ekm1,'fro')]);
-    %     temp = muu*temp/norm(Y,'fro');
-    %     
-    %%%%%%%%%%%%%%%%%%
-    temp1 = max(norm( (X - Zk*Ak),'fro'),norm( (Ak - Bk*Fk),'fro'))/max(norm( Bk*Fk,'fro'),norm( Zk*Ak,'fro'));
-    if temp1 < tol1 && temp < tol2
-    converged = 1;
+    
+    if temp < tol1 
+        converged = 1;
     end
-%     disp(['temp1 ',num2str(temp1)]);
-%     disp([' 迭代次数 ' num2str(iter) ' temp1 ' num2str(temp1) ' temp ' num2str(temp)]);
-    t1(iter)=temp1;
-    t2(iter)=temp;
-%     elapse = cputime - tmp_T;
+    
+%         disp([' 迭代次数 ' num2str(iter) ' temp ' num2str(temp) ]);
 
-  end
-  
-        Z_final = Zkl; %%% Z_final  is finally Z
-        A_final = Akl; %%% A_final  is finally A  
-        B_final = Bkl; %%% B_final  is finally B
-        F_final = Fkl; %%% F_final  is finally F
+    t1(iter)=temp;
+    
 
-        [Z_final,A_final] = NormalizeUV(Z_final, A_final', NormV, Norm);A_final=A_final';
-        [B_final,F_final] = NormalizeUV(B_final, F_final', NormV, Norm);F_final=F_final';
-%         t=1:iter;
-%         figure
-%         plot(t,er,'r-'),xlabel('Iteration times');ylabel('Error');
 end
-
+    Z_final = Z; %%% Z_final  is finally Z
+    A_final = A; %%% A_final  is finally A  
+    B_final = B; %%% B_final  is finally B
+    F_final = F; %%% F_final  is finally F
+        
+end
 function[y] = soft( x, T )
   if sum( abs(T(:)) )==0
        y = x;
